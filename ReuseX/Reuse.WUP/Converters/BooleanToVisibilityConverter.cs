5c74035ff09f51ca9deeaf9159df9245f39d3e0a
keoz <jorge.castro@gigigo.com>
2015-05-29 17:48:37 -0500

    ReuseX bootstrap

---
+++ ReuseX/Reuse.WUP/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+#if WINDOWS_PHONE
+using System.Windows.Data;
+
+#else
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml;
+#endif
+
+namespace Reuse.Common.Converters {
+	/// <summary>
+	/// Converts a bool to Visibility and viceversa, add a parameter to inverse the result.
+	/// </summary>
+	public class BooleanToVisibilityConverter : IValueConverter {
+				
+		#if WINDOWS_PHONE
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			var boolValue = System.Convert.ToBoolean( value );
+
+			if ( parameter != null )
+				boolValue = !boolValue;
+
+			return boolValue ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture ) {
+			return value.Equals( Visibility.Visible );
+		}
+		#else
+		public object Convert( object value, Type targetType, object parameter, string language ) {
+			var boolValue = System.Convert.ToBoolean( value );
+
+			if ( parameter != null )
+				boolValue = !boolValue;
+
+			return boolValue ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, string language ) {
+			return value.Equals( Visibility.Visible );
+		}
+		#endif
+	}
+}
---
+++ ReuseX/Reuse.WUP/Converters/LowerToUpperConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#if WINDOWS_PHONE
+using System.Windows.Data;
+#else
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace Reuse.Common.Converters {
+	public class LowerToUpperConverter : IValueConverter {
+		#if WINDOWS_PHONE
+		public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+			if ( parameter == null ) {
+				return ( value as string ).ToUpper();
+			}
+			return ( value as string ).ToLower();
+		}
+		
+		public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+			throw new NotImplementedException();
+		}
+		#else
+
+		object IValueConverter.Convert( object value, Type targetType, object parameter, string language ) {
+			if ( parameter == null ) {
+				return ( value as string ).ToUpper();
+			}
+			return ( value as string ).ToLower();
+		}
+
+		object IValueConverter.ConvertBack( object value, Type targetType, object parameter, string language ) {
+			throw new NotImplementedException();
+		}
+		#endif
+	}
+}
